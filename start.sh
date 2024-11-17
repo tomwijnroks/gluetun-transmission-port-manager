b8af314757aafe7f539423380a2f2ea301b4de5e
Tom Wijnroks <twx@fastmail.com>
2024-11-17 12:55:45 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,58 +1,80 @@
 #!/bin/bash
 
-COOKIES="/tmp/cookies.txt"
 CURRENT_PORT=""
 
-# Function to update the qbittorrent port
-update_port () {
+# Function to update the transmission port.
+update_port() {
   PORT=$1
 
-  # Clean up cookies file if it exists
-  rm -f "$COOKIES"
+  # Retrieve and set the transmission session id.
+  SESSION_ID=$(
+    curl -s http://${TRANSMISSION_HOST}:${TRANSMISSION_PORT}/transmission/rpc \
+      -u ${TRANSMISSION_USERNAME}:${TRANSMISSION_PASSWORD} | \
+    grep -oP '(?<=X-Transmission-Session-Id: )\w+'
+  )
 
-  # Log in to the qbittorrent web UI and save cookies
-  curl -s -c "$COOKIES" --data "username=$QBITTORRENT_USER&password=$QBITTORRENT_PASS" "${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/auth/login" > /dev/null
-  if [[ $? -ne 0 ]]; then
-    echo "Login failed."
-    return 1
-  fi
+  # Set the forwarded port as peer-port in transmission.
+  curl -s http://${TRANSMISSION_HOST}:${TRANSMISSION_PORT}/transmission/rpc \
+    -u ${TRANSMISSION_USERNAME}:${TRANSMISSION_PASSWORD} \
+    -H "X-Transmission-Session-Id: $SESSION_ID" \
+    -H "Content-Type: application/json" \
+    -d "{\"arguments\": {\"peer-port\": $PORT}, \"method\": \"session-set\"}" \
+    >/dev/null
 
-  # Update qbittorrent preferences with the new port
-  curl -s -b "$COOKIES" --data "json={\"listen_port\": \"$PORT\"}" "${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/app/setPreferences" > /dev/null
- # Check current port to see if changes took effect
-  CURRENT_PORT=$(curl -s -b $COOKIES ${HTTP_S}://${QBITTORRENT_SERVER}:${QBITTORRENT_PORT}/api/v2/app/preferences | jq -r '.listen_port')
+  # Check the current peer-port in transmission.
+  CURRENT_PORT=$(
+    curl -s http://${TRANSMISSION_HOST}:${TRANSMISSION_PORT}/transmission/rpc \
+      -u ${TRANSMISSION_USERNAME}:${TRANSMISSION_PASSWORD} \
+      -H "X-Transmission-Session-Id: $SESSION_ID" \
+      -H "Content-Type: application/json" \
+      -d "{\"arguments\": {\"fields\": [\"peer-port\"]}, \"method\": \"session-get\"}" | \
+    jq ".arguments[\"peer-port\"]"
+  )
 
-  if [ "$CURRENT_PORT" == "$PORT" ]; then
-    echo "Successfully updated qbittorrent to port $PORT"
+  if [ "${CURRENT_PORT}" == "${PORT}" ]; then
+    echo "${DATETIME} - Successfully updated Transmission to port ${PORT}"
     return 0
   else
-    echo "Failed to update port."
+    echo "${DATETIME} - Failed to update port."
     return 1
   fi
 
-  # Clean up cookies file
-  rm -f "$COOKIES"
+  # Unset the transmission session id.
+  SESSION_ID=""
 
-  echo "Successfully updated qbittorrent to port $PORT"
+  # Print info if the transmission port has changed.
+  echo "${DATETIME} Successfully updated Transmission to port ${PORT}"
 }
 
-# Main loop to check the port and update if necessary
 while true; do
-  # Fetch the forwarded port
-  PORT_FORWARDED=$(curl -s ${HTTP_S}://${GLUETUN_HOST}:${GLUETUN_PORT}/v1/openvpn/portforwarded | awk -F: '{gsub(/[^0-9]/,"",$2); print $2}')
-  
-  # Check if the fetched port is valid
-  if [[ -z "$PORT_FORWARDED" || ! "$PORT_FORWARDED" =~ ^[0-9]+$ ]]; then
-    echo "Failed to retrieve a valid port number."
+  # Set the datetime.
+  DATETIME=$(date +"%FT%T")
+
+  # Retrieve the forwarded port from gluetun.
+  PORT_FORWARDED=$(
+    curl -s http://${GLUETUN_HOST}:${GLUETUN_PORT}/v1/openvpn/portforwarded \
+      -u ${GLUETUN_USERNAME}:${GLUETUN_PASSWORD} | \
+    jq '.port'
+  )
+
+  # Check if the forwarded port is a valid port number (1-65535).
+  if [[ -z "${PORT_FORWARDED}" || \
+        ! "${PORT_FORWARDED}" =~ ^[0-9]+$ || \
+        "${PORT_FORWARDED}" -lt 1 || \
+        "${PORT_FORWARDED}" -gt 65535 ]]; then
+    echo "${DATETIME} - Failed to retrieve a valid port number."
     sleep 10
     continue
   fi
 
-  # If the current port is different from the forwarded port, update it
-  if [[ "$CURRENT_PORT" != "$PORT_FORWARDED" ]]; then
-    update_port "$PORT_FORWARDED"
+  # If the current port and forwared port are not equal, execute the update_port function.
+  if [ "${CURRENT_PORT}" != "${PORT_FORWARDED}" ]; then
+    update_port "${PORT_FORWARDED}"
   fi
 
-  # Wait for a specific interval before checking again
-  sleep $RECHECK_TIME
+  # Sleep until the next interval.
+  sleep ${PORT_CHECK_INTERVAL}
+
+  # Unset the datetime.
+  DATETIME=""
 done
